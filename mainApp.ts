8995a82fbe96491a0ceff67fcd7958c3e33e2766
Dc-Valerian <valerianpedro03@gmail.com>
2023-04-13 17:40:33 +0100

    update

--- mainApp.ts
+++ mainApp.ts
@@ -4,7 +4,8 @@ import ejs from "ejs"
 
 
 export const mainApp = (app:Application)=>{
-    app.use(express.json()).use(cors()).set("view engine","ejs").use(express.static("public")).use(express.static(`${__dirname} public/css`)).use(express.static(`${__dirname} public/asset`))
+    app.
+    use(express.json()).use(cors()).set("view engine","ejs").use(express.static("public")).use(express.static(`${__dirname} ./public/css`)).use(express.static(`${__dirname} ./public/asset`))
 
     app.get("/view",(req:Request,res:Response)=>{
 res.render("index")
