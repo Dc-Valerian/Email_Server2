f515bc2fca21fd47beb031a1d44b701413b4a995
Dc-Valerian <valerianpedro03@gmail.com>
2023-04-13 17:47:31 +0100

    update

--- mainApp.ts
+++ mainApp.ts
@@ -2,13 +2,18 @@ import express, { Application,Request,Response } from "express"
 import cors from "cors"
 import ejs from "ejs"
 
+const data={
+    name:"Valerian",
+    matID:33445,
+    color:"red"
+}
 
 export const mainApp = (app:Application)=>{
     app.
     use(express.json()).use(cors()).set("view engine","ejs").use(express.static("public")).use(express.static(`${__dirname} ./public/css`)).use(express.static(`${__dirname} ./public/asset`))
 
     app.get("/view",(req:Request,res:Response)=>{
-res.render("index")
+res.render("index",{data})
     })
     app.get("/",(req:Request,res:Response)=>{
 res.json({
